2b61e681e6f23d4fbfdafb2d99ee8c105ac64254
Sam Meech-Ward <sam@meech-ward.com>
2017-07-07 09:21:08 -0700

    Initial Commit

---
+++ GCDDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GCDDemo
+//
+//  Created by Sam Meech-Ward on 2017-07-07.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
